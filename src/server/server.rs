d34dbce6dcb3079e9da8c838bb730bcccb96798a
Maduki-tech <d.schlueter1011@gmail.com>
2023-12-31 13:25:53 +0100

    moved function outside

--- src/server/server.rs
+++ src/server/server.rs
@@ -15,12 +15,16 @@ impl Server {
     
     #[tokio::main]
     pub async fn start(&self) {
-        let app = Router::new().route("/", get(|| async {"Hello, World!"}));
+        let app = Router::new().route("/", get(self.get_handler()));
         let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
         axum::serve(listner, app).await.unwrap();
-        
     }
+
+    fn get_handler(&self) -> String {
+        "hello world".to_string()
+    }
+
+
 }
 
 
